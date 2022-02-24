a56f98842d9ecbba69a752004cf1a40b36e4f0e8
hilolaar <aripovah14@gmail.com>
2022-02-24 18:32:26 +0600

    commit -m

---
+++ 22-02/script.js
@@ -0,0 +1,19 @@
+let numOne = 3;
+alert(numOne);
+
+let number_two = 2;
+alert(number_two);
+
+let NumberThree = 1;
+alert(NumberThree);
+
+let str1 = 'Салам ';
+let str2 = ', Бишкек!';
+alert(str1 + str2);
+
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result);
