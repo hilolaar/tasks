20e9b657b88b450d32abd04eb4c18118d741113c
hilolaar <aripovah14@gmail.com>
2022-03-05 17:40:13 +0600

    commit -m

---
+++ 04-03/script.js
@@ -0,0 +1,18 @@
+let arr = ['a','b','c','d'];
+//let str = `${arr[0]}+${arr[1]}, ${arr[2]}+${arr[3]}`;
+
+let str2 = arr[0] + '+' + arr[1] + ', ' + arr[2] + '+' + arr[3];
+console.log(str2);
+
+let numbers = [10, 15, 9, 10];
+let a = numbers[0] * numbers[1];
+let b = numbers[2] * numbers [3];
+
+let result = a + b;
+console.log(result);
+
+let arr2 = ['a', 'b', 'c'];
+arr2[2] = 1 ;
+arr2[3] = 2;
+arr2[5] = 3;
+console.log(arr2)
