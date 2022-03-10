e120c6865ac5bc05344bb783ba734b2614128832
hilolaar <aripovah14@gmail.com>
2022-03-10 19:15:30 +0600

    commit -m

---
+++ 05-03/script.js
@@ -0,0 +1,41 @@
+//1
+let numbers = [1,2,8,4,5,3,7,9,11,14];
+for(let i = 0; i < 10; i++){
+    if(numbers[i] >= 10){
+        continue;
+    }
+    console.log(numbers[i]);
+}
+
+//2
+const number = [1,2,5,6,88,5];
+let result = 0;
+for (let i = 0; i < number.length; i++){
+    result += number[i];
+}
+console.log(result);
+
+
+//3
+let arr = [2, 5, 6, 7,66,11,23,19,4,3,13];
+let square = 0;
+for(let i = 0; i < arr.length; i++){
+    square += arr[i] * arr[i];
+}
+console.log(square);
+
+//4
+let arr1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+for(let i = 0; i <= 20; i++){
+    if( i % 2 == 0){
+        console.log(i);
+    }
+}
+
+//5
+let a = [];
+for(i = 0; i <= 9; i++){
+    a[i] = 'x';
+}
+console.log(a);
+
