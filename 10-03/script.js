2a374dcff5810eb8196706c7b97f8888ed246e90
hilolaar <aripovah14@gmail.com>
2022-03-12 20:16:10 +0600

    init

---
+++ 10-03/script.js
@@ -0,0 +1,17 @@
+function min(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+console.log(min(5, 3));
+
+function showNumber(num) {
+    alert(num);
+}
+
+showNumber(2);
+
+
