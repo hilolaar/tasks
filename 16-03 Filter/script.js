253e2df6b7cf6333c76cc6d528ff6bde276578bc
hilolaar <aripovah14@gmail.com>
2022-03-17 20:29:05 +0600

    init: edited js

--- 16-03 Filter/script.js
+++ 16-03 Filter/script.js
@@ -27,12 +27,32 @@ const goods = [
 
 //filterPrice(arr, 800 , <);
 
+const filterPrice = (goodsArray, price, operator) => {
+    return goodsArray.filter(good =>{
+        if(operator === '>'){
+            return good.price > price;
+        } else if(operator === '<'){
+            return good.price < price;
+        } else {
+            return 'Параметр operator неизвестен';
+        }
+
+    })
+}
+
+console.log(filterPrice(goods, 1000, '<'))
+
+
+
+
 /*const filter1 = goods.filter(function (currentValue) {
     return currentValue.price < 800;
 })
 console.log(filter1);*/
 
 
+/*
 const filter2 = goods.filter(currentValue => currentValue.price  < 800);
 console.log(filter2);
+*/
 
