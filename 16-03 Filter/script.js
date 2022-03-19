0e8edbf8971cf8abc916d1758f8224d39e3a8bb7
hilolaar <aripovah14@gmail.com>
2022-03-19 18:59:16 +0600

    init: added js

--- 16-03 Filter/script.js
+++ 16-03 Filter/script.js
@@ -1,3 +1,4 @@
+/*
 const goods = [
     {
         name:'Iphone 12',
@@ -33,26 +34,31 @@ const filterPrice = (goodsArray, price, operator) => {
             return good.price > price;
         } else if(operator === '<'){
             return good.price < price;
-        } else {
-            return 'Параметр operator неизвестен';
         }
 
     })
 }
-
 console.log(filterPrice(goods, 1000, '<'))
 
+/*function filteredPrice(array, benchmark, symbol){
+    return goods.filter(array => (symbol === '<') ? array.price < benchmark : array.price > benchmark);
+}
+console.log(filteredPrice([goods], 1000, '>'));*!/
 
+const filter = (arr) => {
+    let group1 = [];
+    let group2 = [];
 
-
-/*const filter1 = goods.filter(function (currentValue) {
-    return currentValue.price < 800;
-})
-console.log(filter1);*/
-
-
-/*
-const filter2 = goods.filter(currentValue => currentValue.price  < 800);
-console.log(filter2);
-*/
-
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].price > 800){
+            group2.push(arr[i].name);
+        } else if(arr[i].price < 800){
+            group1.push(arr[i].name);
+        }
+    }
+    return{
+        group1, // group1:group1
+        group2 // group2:group2
+    }
+}
+console.log(filter(goods));*/
---
+++ 17-03/script.js
@@ -0,0 +1,63 @@
+let arr = [10, 5, -5, 55, -1, 22, -4, 36, -45];
+let result = arr.reduce(function (sum, elem) {
+    if(elem >= 0){
+        return  sum + elem;
+    } else {
+        return sum;
+    }
+
+});
+
+console.log(result);
+
+/**** Описание *****/
+
+/*
+(elem >= 0){
+    return elem + sum;
+
+1) sum = 0 ; elem = -5;
+  -5 >= 0 //false
+   sum = 0
+
+2) sum = 0; elem = 10;
+  10 >= 0 //true
+  0 + 10 = 10; => sum = 10
+
+3) sum = 10; elem = 5;
+   5 >= 0 //true
+10 + 5 = 15; => sum = 15
+
+4) sum = 15; elem = 55
+   55 >= 0 //true
+   15 + 55 = 70; => sum = 70
+
+5) sum = 70; elem = -1;
+   -1 >= 0 //false
+   sum = 70
+
+6) sum = 70; elem = 22
+   22 >= 0 //true
+   70 + 22 = 92; => sum = 92
+
+7)  sum = 92; elem = -4;
+   -4 >= 0 //false
+   sum = 92
+
+8) sum = 92 ; elem = 36
+   36 >= 0 //true
+   92 + 36 = 128;
+
+9) sum = 128; elem = -45;
+   -45 >= 0 //false
+   sum = 128
+*/
+
+function myFunc(value, length) {
+    const arr = [];
+    for (var i = 0; i < length; i++) {
+        arr.push(value);
+    }
+    return arr;
+}
+console.log(myFunc('a', 3));
