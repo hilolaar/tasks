a45346a1f06dd960ffe335829392f9ab9264644b
hilolaar <aripovah14@gmail.com>
2022-03-24 16:59:00 +0600

    init: edited js

---
+++ 22-03/script.js
@@ -0,0 +1,27 @@
+//1
+
+let numbers = prompt('Введите число');
+let n = 0;
+
+   while(numbers != 99){
+       n++;
+       numbers = prompt('Введите число');
+}
+
+console.log('Количество чисел - ' + n);
+
+//2
+
+/*
+let n = 0;
+let sum = 0;
+let count = Number(prompt('Введите число'));
+
+   while(count != 999){
+      n++;
+      sum += count;
+}
+console.log('Количество чисел - ' + n ,'Сумма чисел = ' + sum);
+*/
+
+
